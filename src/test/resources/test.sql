449c3a6a96257e59284cff2c8a11e30461cda7d1
Praveen <praveenk1029@gmail.com>
2020-01-05 19:34:25 -0500

    Added h2 database for integration testing.

---
+++ src/test/resources/test.sql
@@ -0,0 +1,12 @@
+insert into department values (1, 'Information Technology');
+insert into department values (2, 'Marketing');
+insert into department values (3, 'Sales');
+insert into department values (4, 'Human Resources');
+insert into department values (5, 'Administration');
+
+insert into employee values (1, 'Tom', 'Victor', 5438.26, 2);
+insert into employee values (2, 'Allen', 'Williams', 4532.86, 1);
+insert into employee values (3, 'James', 'Munich', 5682.68, 2);
+
+
+
