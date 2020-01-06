911b9e0d222ccb0587e1a8f34ee5e0da27b1ed11
Praveen <praveenk1029@gmail.com>
2020-01-05 19:01:45 -0500

    Added test cases.

--- src/test/java/com/practice/springbootdbexample/SpringbootDbExampleApplicationTests.java
+++ src/test/java/com/practice/springbootdbexample/SpringbootDbExampleApplicationTests.java
@@ -1,16 +1,47 @@
 package com.practice.springbootdbexample;
 
+import com.practice.springbootdbexample.controller.EmployeeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class SpringbootDbExampleApplicationTests {
 
+	@Autowired
+	protected TestRestTemplate testRestTemplate;
+	HttpHeaders httpHeaders = new HttpHeaders();
+
+	@LocalServerPort
+	private int port;
+
 	@Test
-	public void contextLoads() {
+	@Sql("/test.sql")
+	public void testGetEmployee(){
+		HttpEntity<String> entity = new HttpEntity<>(null, httpHeaders);
+		ResponseEntity<String> response = testRestTemplate
+				.exchange(createURLWithPort("/rest/employee/getEmployee"),
+						HttpMethod.GET, entity, String.class);
+		System.out.println("response.getHeaders()::::"+response.getHeaders());
+		System.out.println("response.getBody():::::::"+response.getBody());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+
+	private String createURLWithPort(String uri) {
+		return "http://localhost:" + port + uri;
 	}
 
 }
